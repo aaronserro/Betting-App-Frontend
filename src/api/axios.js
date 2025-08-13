22ba1e244ab0df7b12581f8e5231c4d738fb4c2f
Aaron Basil Serro <aserro2@uwo.ca>
2025-08-13 10:08:19 -0400

    final check

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
- baseURL: 'http://localhost:8080',
+ baseURL: import.meta.env.VITE_API_BASE ?? 'http://localhost:8080',
  //baseURL:'https://app-backend-c0t6.onrender.com',
   headers: {
     'Content-Type': 'application/json',
